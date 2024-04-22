d3c05b4763227c361e36c145869b7d7ea50a607e
estudiodigiacomo <bruno@estudiodigiacomo.com>
2024-04-22 15:56:14 -0300

    setup preferences

--- src/gui/online_voucher_wind.py
+++ src/gui/online_voucher_wind.py
@@ -1,14 +1,15 @@
 import tkinter as tk
-from datetime import datetime
-from tkcalendar import DateEntry
 from read_sheet_afip import get_clients_from_sheets
 from login_afip import login_afip
 from online_vouchers import online_voucher
+from tkinter import ttk
+from tkcalendar import DateEntry
+from nav_preferences import setup_nav_preferences 
 
 def open_online_vouchers_window(client_name):
     online_vouchers_window = tk.Tk()
     online_vouchers_window.title('Comprobantes en Línea')
-    online_vouchers_window.geometry('350x200')
+    online_vouchers_window.geometry('400x200')
 
     clients = get_clients_from_sheets()
     for client in clients:
@@ -20,7 +21,7 @@ def open_online_vouchers_window(client_name):
 
     type_options = ['seleccionar...', 'Factura A', 'Nota de Débito A', 'Nota de Crédito A', 'Recibo A', 'Factura B', 'Nota de Débito B', 'Nota de Crédito B', 'Recibo B', 'Factura C', 'Nota de Débito C', 'Nota de Crédito C', 'Recibo C', 'Factura de Exportación E', 'Nota de Débito por Operaciones con el Exterior E', 'Nota de Crédito por Operaciones con el Exterior E', 'Comprobante de Compra de Bienes Usados', 'Factura M', 'Nota de Débito M', 'Nota de Crédito M', 'Recibo M', 'Factura T', 'Nota de Débito T', 'Nota de Crédito T', 'Factura de Crédito Electrónica MiPyMEs (FCE) A', 'Nota de Débito Electrónica MiPyMEs (FCE) A', 'Nota de Crédito Electrónica MiPyMEs (FCE) A', 'Factura de Crédito Electrónica MiPyMEs (FCE) B', 'Nota de Débito Electrónica MiPyMEs (FCE) B', 'Nota de Crédito Electrónica MiPyMEs (FCE) B', 'Factura de Crédito Electrónica MiPyMEs (FCE) C', 'Nota de Débito Electrónica MiPyMEs (FCE) C', 'Nota de Crédito Electrónica MiPyMEs (FCE) C']
 
-
+    #Tipo de comprobante
     type_label = tk.Label(online_vouchers_window, text="Tipo:")
     type_label.grid(row=2, column=0, padx=5, pady=5, sticky='w')
     type_var = tk.StringVar()
@@ -30,33 +31,33 @@ def open_online_vouchers_window(client_name):
 
     points_of_sale = ['seleccionar...', pto_venta1, pto_venta2, pto_venta3, pto_venta4]
 
+    #Punto de venta
     point_sale_label = tk.Label(online_vouchers_window, text="Punto de Venta:")
     point_sale_label.grid(row=3, column=0, padx=5, pady=5, sticky='w')
-
     point_sale_var = tk.StringVar()
     point_sale_var.set(points_of_sale[0])
     sale_option_menu = tk.OptionMenu(online_vouchers_window, point_sale_var, *points_of_sale)
     sale_option_menu.grid(row=3, column=1, padx=5, pady=5, sticky='w')
-        
-    cal_from = DateEntry(online_vouchers_window, width=12, background='darkblue', foreground='white', borderwidth=2, date_pattern='dd/mm/yyyy')
-    cal_from.grid(row=4, column=0, padx=5, pady=5, sticky='w')
 
-    cal_to = DateEntry(online_vouchers_window, width=12, background='darkblue', foreground='white', borderwidth=2, date_pattern='dd/mm/yyyy')
-    cal_to.grid(row=4, column=1, padx=5, pady=5, sticky='w')
+    #Fecha desde
+    date_from_label = tk.Label(online_vouchers_window, text='Desde: ')
+    date_from_label.grid(row=4, column=0, padx=5, pady=5, sticky='w')
+    date_from_entry = DateEntry(online_vouchers_window, width=12, date_pattern="dd/mm/yyyy")
+    date_from_entry.grid(row=4, column=1, padx=5, pady=5, sticky='w')
+    #Fecha desde
+    date_to_label = tk.Label(online_vouchers_window, text="Hasta:")
+    date_to_label.grid(row=5, column=0, padx=5, pady=5, sticky='w')
+    date_to_entry = DateEntry(online_vouchers_window, width=12, date_pattern="dd/mm/yyyy")
+    date_to_entry.grid(row=5, column=1, padx=5, pady=5, sticky='w')
 
-    # Agregando selector de fechas
-    start_date = cal_from.get_date()
-    end_date = cal_to.get_date()
-    start_date_str =start_date.strftime('%d/%m/%Y')
-    end_date_str = end_date.strftime('%d/%m/%Y')
+    #Inicio de automatizacion
+    btn_login = tk.Button(online_vouchers_window, text='Iniciar automatizacion', command=lambda: login_and_open_vouchers(client_name, " ",type_var.get(), point_sale_var.get(), date_from_entry.get(), date_to_entry.get()))
 
-    print(client_name, type_var.get(), point_sale_var.get(), start_date_str, end_date_str)
-
-    btn_login = tk.Button(online_vouchers_window, text='Iniciar automatizacion', command=lambda: login_and_open_vouchers(client_name, type_var.get(), point_sale_var.get(), start_date_str, end_date_str))
-
-    btn_login.grid(row=3, column=0, columnspan=2, padx=5, pady=5, sticky='w')
+    btn_login.grid(row=6, column=0, columnspan=2, padx=5, pady=5, sticky='w')
     online_vouchers_window.mainloop()
 
-def login_and_open_vouchers(client_name, type_voucher, type_var, point_sale_var, start_date_str, end_date_str):
+def login_and_open_vouchers(client_name, type_voucher, type_var, point_sale_var, date_from, date_to):
     login_afip(client_name, type_voucher)
-    online_voucher(client_name, type_var, point_sale_var, start_date_str, end_date_str)
+    print(client_name, type_var, point_sale_var, date_from, date_to)
+    online_voucher(client_name, type_var, point_sale_var, date_from, date_to)
+    setup_nav_preferences(type_voucher)
